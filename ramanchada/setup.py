1512770225267260b90749508c9b71aaa5943067
vedina <jeliazkova.nina@@gmail.com>
2021-05-18 13:37:59 +0300

    renamed

--- ramanchada/setup.py
+++ ramanchada/setup.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 
-from setuptools import setup
+from setuptools import setup, find_packages
 
 with open("README.md", "r") as fs:
     long_description = fs.read()
@@ -34,13 +34,10 @@ setup(
     ],
     keywords="Raman spectra standardisation",
     package_dir={"": "src"},
-    packages=setuptools.find_packages(where="src"),
+    packages=find_packages(where="src"),
     python_requires="~=3.5",
-    packages=[
-        "ramanchada",
-    ],
     install_requires=[
         "pandas",
         "scipy"
-    ],
+    ]
 )
