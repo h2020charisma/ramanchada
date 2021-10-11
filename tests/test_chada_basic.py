5b86399d1f400f28b4a6fb38a98d9b13655a6b1f
vedina <jeliazkova.nina@gmail.com>
2021-10-11 21:24:32 +0300

    make flake8 happy

--- tests/test_chada_basic.py
+++ tests/test_chada_basic.py
@@ -1,7 +1,7 @@
 from pathlib import Path
-
 from ramanchada.classes import RamanChada
 import os
+
 
 def get_native_file(filename='200218-17.wdf'):
     TEST_FILE_DIR = Path('test')
@@ -9,18 +9,20 @@ def get_native_file(filename='200218-17.wdf'):
     assert TEST_FILE.exists()
     return TEST_FILE
 
+
 def get_chada_file(filename='200218-17.cha'):
     TEST_FILE_DIR = Path('test')
     TEST_FILE = TEST_FILE_DIR / filename
     return TEST_FILE
 
-#This tests if RamanChada can load a file
+
+# This tests if RamanChada can load a file
 def test_load_data_file_into_object():
-    #make sure .cha file does not exist (see issue 1)
+    # make sure .cha file does not exist (see issue 1)
     chada_file = get_chada_file()
     if chada_file.exists():
         os.remove(chada_file)
-    #    
+
     native_file = get_native_file()
     chada_obj = RamanChada(native_file)
     assert chada_obj.x.shape == (3190,)
