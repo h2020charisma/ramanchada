90d0e55e4d70408a4704a3a7dd2b5717bde16a9f
Luchesar ILIEV <luchesar.iliev@gmail.com>
2021-10-14 16:00:04 +0300

    CI: fix cleanup in test_chada_basic

--- tests/test_chada_basic.py
+++ tests/test_chada_basic.py
@@ -16,13 +16,8 @@ def get_chada_file(filename='200218-17.cha'):
     return TEST_FILE
 
 
-# This tests if RamanChada can load a file
+# This tests if RamanChada can load a file.
 def test_load_data_file_into_object():
-    # make sure .cha file does not exist (see issue 1)
-    # chada_file = get_chada_file()
-    # if chada_file.exists():
-    #   os.remove(chada_file)
-
     native_file = get_native_file()
     chada_obj = RamanChada(native_file)
     assert chada_obj.x.shape == (3190,)
@@ -38,5 +33,7 @@ def test_load_data_file_into_object():
     chada_obj.peaks(fitmethod='vg')
     print(chada_obj.bands)
 
-    if native_file.exists():
-        os.remove(native_file)
+    # Clean up.
+    chada_file = get_chada_file()
+    if chada_file.exists():
+        os.remove(chada_file)
