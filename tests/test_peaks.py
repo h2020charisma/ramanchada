ebf66a64e6c07ffbd599b2459851181718679f09
Luchesar ILIEV <luchesar.iliev@gmail.com>
2021-10-14 15:37:08 +0300

    CI: fix lint errors

--- tests/test_peaks.py
+++ tests/test_peaks.py
@@ -16,24 +16,25 @@ def test_peaks():
     C = chada.Chada(chada_file)
     assert C.x_data.shape == (3190,)
     C.peaks()
-    assert abs(C.bands.at[5,"position"] - 1615.631) < 1E-3
-    assert abs(C.bands.at[6,"position"] - 1727.773) < 1E-3
-    assert abs(C.bands.at[8,"position"] - 3083.742) < 1E-3
-    assert abs(C.bands.at[5,"FWHM"] - 9.335) < 1E-3
-    assert abs(C.bands.at[6,"FWHM"] - 13.084) < 1E-3
-    assert abs(C.bands.at[8,"FWHM"] - 13.561) < 1E-3    
+    assert abs(C.bands.at[5, "position"] - 1615.631) < 1E-3
+    assert abs(C.bands.at[6, "position"] - 1727.773) < 1E-3
+    assert abs(C.bands.at[8, "position"] - 3083.742) < 1E-3
+    assert abs(C.bands.at[5, "FWHM"] - 9.335) < 1E-3
+    assert abs(C.bands.at[6, "FWHM"] - 13.084) < 1E-3
+    assert abs(C.bands.at[8, "FWHM"] - 13.561) < 1E-3
+
 
 def test_peaks_with_baseline():
     chada_file = chada_io.create(init_data_file())
     C = chada.Chada(chada_file)
     assert C.x_data.shape == (3190,)
     C.fit_baseline(show=True)
-    C.remove_baseline()    
+    C.remove_baseline()
     C.peaks()
-    assert abs(C.bands.at[5,"position"] - 1615.631) < 1E-3
-    assert abs(C.bands.at[6,"position"] - 1727.773) < 1E-3
-    assert abs(C.bands.at[8,"position"] - 3083.742) < 1E-3
+    assert abs(C.bands.at[5, "position"] - 1615.631) < 1E-3
+    assert abs(C.bands.at[6, "position"] - 1727.773) < 1E-3
+    assert abs(C.bands.at[8, "position"] - 3083.742) < 1E-3
 
-    assert abs(C.bands.at[5,"FWHM"] - 9.328) < 1E-3
-    assert abs(C.bands.at[6,"FWHM"] - 13.057) < 1E-3
-    assert abs(C.bands.at[8,"FWHM"] - 13.563) < 1E-3  
+    assert abs(C.bands.at[5, "FWHM"] - 9.328) < 1E-3
+    assert abs(C.bands.at[6, "FWHM"] - 13.057) < 1E-3
+    assert abs(C.bands.at[8, "FWHM"] - 13.563) < 1E-3
