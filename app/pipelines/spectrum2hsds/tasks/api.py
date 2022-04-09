fa0540cd188ab57e07f5859f75aafe0af94ecdbd
vedina <jeliazkova.nina@gmail.com>
2022-04-09 18:18:34 +0300

    ploomber pipeline refactored to take more than one set of spectra at once config.json defines what will be processed , could be a command line parameter to ploomber

--- app/pipelines/spectrum2hsds/tasks/api.py
+++ app/pipelines/spectrum2hsds/tasks/api.py
@@ -1,15 +1,13 @@
 # + tags=["parameters"]
-upstream = ["tasks.metadata.get"]
+upstream = ["tasks.metadata.read_metadata"]
 product = None
-folder_input = None
-json_metadata = None
+config_input = None
+metadata_root = None
 ramandb_api = None
 hs_username = None
 hs_password = None
 hsds_investigation = None
-hsds_provider = None
-hsds_instrument = None
-hsds_wavelength = None
+
 # -
 
 import json
@@ -19,7 +17,7 @@ import requests
 from pprint import pprint
 import glob 
 
-def submit2hsds(file_name,json_metadata,ramandb_api,hs_username,hs_password,
+def submit2hsds(file_name,ramandb_api,hs_username,hs_password,
                 hsds_investigation,hsds_provider,hsds_instrument,hsds_wavelength,optical_path,sample,laser_power,metadata):
     domain = "/{}/{}/{}/{}/".format(hsds_investigation,hsds_provider,hsds_instrument,hsds_wavelength)
     api_dataset = "{}dataset?domain={}".format(ramandb_api,domain);
@@ -39,10 +37,8 @@ def submit2hsds(file_name,json_metadata,ramandb_api,hs_username,hs_password,
     
 import pandas as pd
 
-def files2hsds(folder_input,json_metadata,ramandb_api,hs_username,hs_password,hsds_investigation,hsds_provider,hsds_instrument,hsds_wavelength,product):
-    with open(json_metadata, 'r') as infile:
-        metadata = json.load(infile)
-
+def files2hsds(metadata,ramandb_api,hs_username,hs_password,hsds_investigation,hsds_provider,hsds_instrument,hsds_wavelength,log_file):
+    folder_input = metadata["folder_input"]
     log = {"results" : {}}
     spectra_files = glob.glob(os.path.join(folder_input,"**","*"), recursive=True)
     file_lookup = {}
@@ -68,11 +64,29 @@ def files2hsds(folder_input,json_metadata,ramandb_api,hs_username,hs_password,hs
                         sample = files["sample"] 
                         op_id = op["id"]
                         print(sample,op_id,laser_power,file_name)
-                    submit2hsds(file_name,json_metadata,ramandb_api,hs_username,hs_password,
+                    submit2hsds(file_name,ramandb_api,hs_username,hs_password,
                         hsds_investigation,hsds_provider,hsds_instrument,hsds_wavelength,
                             op_id,sample,laser_power,log["results"])
             
 
     log["files"] = file_lookup
-    with open(product["data"], "w",encoding="utf-8") as write_file:
+    with open(log_file, "w",encoding="utf-8") as write_file:
         json.dump(log, write_file, sort_keys=True, indent=4)    
+
+def folders2hsds(config_input,metadata_root,ramandb_api,hs_username,hs_password,hsds_investigation,product):
+    
+    with open(config_input, 'r') as infile:
+        config = json.load(infile)
+    for entry in config:
+        if entry["enabled"]:
+            json_metadata = os.path.join(metadata_root,"metadata_{}_{}_{}.json".
+                format(entry["hsds_provider"],entry["hsds_instrument"],entry["hsds_wavelength"]))
+            log_file = os.path.join(product["data"],"log_{}_{}_{}.json".
+                format(entry["hsds_provider"],entry["hsds_instrument"],entry["hsds_wavelength"]))
+            with open(json_metadata, 'r') as infile:
+                metadata = json.load(infile)
+
+            files2hsds(metadata,ramandb_api,hs_username,hs_password,
+                    hsds_investigation,entry["hsds_provider"],entry["hsds_instrument"],
+                    entry["hsds_wavelength"],log_file)
+    
--- app/pipelines/spectrum2hsds/tasks/metadata.py
+++ app/pipelines/spectrum2hsds/tasks/metadata.py
@@ -1,15 +1,17 @@
 # + tags=["parameters"]
 upstream = None
 product = None
-folder_input = None
-file_metadata = None
+root_folder = None
+config_input = None
 # -
 
+from re import L
 import pandas as pd
 import os,path
 import json
 
 def read_opticalpath(sheet_name,file_metadata):
+    
     tag_sample=["S0N","S0B","S0P","S1N","nCAL","sCAL","PST","Sil"]
     op_sheet = pd.read_excel(file_metadata,sheet_name=sheet_name,header=None)
     start_row = 29
@@ -17,24 +19,29 @@ def read_opticalpath(sheet_name,file_metadata):
     tmp = []
     try:
         while not pd.isna(op_sheet.iloc[start_row][0]):
-            #print(start_row)
+            
             for sample in range(0,len(tag_sample),1):
                 file_name = op_sheet.iloc[start_row+sample][11]
                 if not pd.isna(file_name):
                     files = file_name.split(",")
                     tmp.append({"sample" : tag_sample[sample],"file" : files , "laser_power" : op_sheet.iloc[start_row][4]})
             start_row = start_row + offset_row
-
+            if start_row>op_sheet.shape[0]:
+                break
     except Exception as err:
+        print(err,sheet_name)
         pass
+    
     return tmp
     
-def get(folder_input,file_metadata,product):
+def get(folder_input,file_metadata,product_metadata):
+
     front_sheet = pd.read_excel(file_metadata,sheet_name="Front sheet",header=None)
     front_sheet.fillna("",inplace=True)
-    print(front_sheet.columns)
     
-    metadata={ "provider" : front_sheet.iloc[0][1], 
+    metadata={  "folder_input" : folder_input,
+                "file_metadata" : file_metadata,
+                "provider" : front_sheet.iloc[0][1], 
                "instrument" : front_sheet.iloc[0][6], "wavelength" : front_sheet.iloc[1][6],
                "optical_path" : [] }
     
@@ -60,6 +67,20 @@ def get(folder_input,file_metadata,product):
     except Exception as err:
         pass
         
-
-    with open(product["data"], "w",encoding="utf-8") as write_file:
+    with open(product_metadata, "w",encoding="utf-8") as write_file:
         json.dump(metadata, write_file, sort_keys=True, indent=4)    
+
+
+def read_metadata(root_folder,config_input,product):
+
+    with open(config_input, 'r') as infile:
+        config = json.load(infile)
+    for entry in config:
+        if entry["enabled"]:
+            product_metadata = os.path.join(product["data"],"metadata_{}_{}_{}.json".
+                format(entry["hsds_provider"],entry["hsds_instrument"],entry["hsds_wavelength"]))
+
+            ##_root = os.path.join(root_folder,entry["folder_input"]) #not sure why it doesn't work and strips the win part to d:\
+            folder_input = os.path.join(root_folder,entry["folder_input"])
+            file_metadata = os.path.join(root_folder,entry["file_metadata"])
+            get(folder_input,file_metadata,product_metadata)
